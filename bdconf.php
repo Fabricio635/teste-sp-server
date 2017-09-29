4ad58524188adf1468a2db082f6569289bd791f3
Fabricio635 <fabricio635@gmail.com>
2017-09-28 20:24:14 -0400

    db openshift

--- bdconf.php
+++ bdconf.php
@@ -7,7 +7,7 @@ $conn = mysql_select_db(env('MYSQL_DATABASE', ''), $conn)  or die ('Não foi pos
 
 $host = getenv('MYSQL_SERVICE_HOST');
 $port = getenv('MYSQL_SERVICE_PORT');
-$conn = @mysql_connect("$host:$port", getenv('MYSQL_USER'), getenv('MYSQL_PASSWORD') or die (mysql_error());
+$conn = @mysql_connect("$host:$port", getenv('MYSQL_USER'), getenv('MYSQL_PASSWORD')) or die (mysql_error());
 $conn = mysql_select_db(getenv('MYSQL_DATABASE'), $conn)  or die ('Não foi possivel conectar ao banco de dados...');
 
 ?>
