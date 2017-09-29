dde1d4c7776201eb93bd853738e2867ebf8182c3
Fabricio635 <fabricio635@gmail.com>
2017-09-28 22:54:07 -0400

    erros corrigidos

--- bdconf.php
+++ bdconf.php
@@ -1,13 +1,8 @@
 <?php
 
-/*$host = env('MYSQL_SERVICE_HOST', '');
-$port = env('MYSQL_SERVICE_PORT', '');
-$conn = @mysql_connect("$host:$port", env('MYSQL_USER', ''), env('MYSQL_PASSWORD', '')) or die (mysql_error());
-$conn = mysql_select_db(env('MYSQL_DATABASE', ''), $conn)  or die ('Não foi possivel conectar ao banco de dados...');*/
-
-$host = getenv(strtoupper(getenv("DATABASE_SERVICE_NAME"))."_SERVICE_HOST");
-$port = getenv(strtoupper(getenv("DATABASE_SERVICE_NAME"))."_SERVICE_PORT");
-$conn = @mysql_connect("$host:$port", getenv("DATABASE_USER"), getenv("DATABASE_PASSWORD")) or die (mysql_error());
-$conn = mysql_select_db(getenv("DATABASE_NAME"), $conn)  or die ('Não foi possivel conectar ao banco de dados...');
+$host = "localhost";
+$port = "3306";
+$conn = @mysql_connect($host, "usuario", "senha") or die (mysql_error());
+$conn = mysql_select_db("br", $conn)  or die ('Não foi possivel conectar ao banco de dados...');
 
 ?>
